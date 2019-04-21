d4e04f847c9aacc199ff08b47ee2cb5b3182fee6
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-04-21 16:07:20 +0900

    synchronize-star-count-with-refetch-queries

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import client from "./client";
 
 const StarButton = props => {
   console.log(props.node.stargazers.totalCount);
-  const node = props.node;
+  const { node, query, first, last, before, after } = props;
   const totalCount = node.stargazers.totalCount;
   const viewerHasStarred = node.viewerHasStarred;
   const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`;
@@ -24,7 +24,18 @@ const StarButton = props => {
   };
 
   return (
-    <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}>
+    <Mutation
+      mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
+      refetchQueries={mutationResult => {
+        console.log({ mutationResult });
+        return [
+          {
+            query: SEARCH_REPOSITORIES,
+            variables: { query, first, last, before, after }
+          }
+        ];
+      }}
+    >
       {addOrRemoveStar => <StarStatus addOrRemoveStar={addOrRemoveStar} />}
     </Mutation>
   );
@@ -112,7 +123,10 @@ class App extends Component {
                           {node.name}
                         </a>
                         &nbsp;
-                        <StarButton node={node} />
+                        <StarButton
+                          node={node}
+                          {...{ query, first, last, after, before }}
+                        />
                       </li>
                     );
                   })}
